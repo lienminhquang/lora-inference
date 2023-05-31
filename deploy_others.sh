0c120cb9a360fae62fe3632a1b30ef434fc5aabc
quanglm <minhquang130698@gmail.com>
2023-05-31 15:57:22 +0700

    rv 2.0

--- deploy_others.sh
+++ deploy_others.sh
@@ -1,8 +1,8 @@
-export MODEL_ID="SG161222/Realistic_Vision_V1.3" # change this
+export MODEL_ID="SG161222/Realistic_Vision_V2.0" # change this
 export SAFETY_MODEL_ID="CompVis/stable-diffusion-safety-checker"
 export IS_FP16=1
-export USERNAME="cloneofsimo" # change this
-export REPLICATE_MODEL_ID="realistic_vision_v1.3" # change this
+export USERNAME="purrfectai" # change this
+export REPLICATE_MODEL_ID="realistic_vision_v2.0" # change this
 
 echo "MODEL_ID=$MODEL_ID" > .env
 echo "SAFETY_MODEL_ID=$SAFETY_MODEL_ID" >> .env
